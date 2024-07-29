927172ea316ef04f83d82060569ef40b17c5c685
R-light07 <110286868+R-light07@users.noreply.github.com>
2024-07-29 09:18:13 +0200

    Update script.js

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,30 +1,55 @@
 function Calculator() {
-    return (
-      <div className="calculator">
-        <div className="display">0</div>
-        <CalcButton value="7"/>
-        <CalcButton value="8"/>
-        <CalcButton value="9"/>
-        <CalcButton className="operator" value="/"/>
-        
-        <CalcButton value="6"/>
-        <CalcButton value="5"/>
-        <CalcButton value="4"/>
-        <CalcButton className="operator" value="*"/>
-        
-        <CalcButton value="3"/>
-        <CalcButton value="2"/>
-        <CalcButton value="1"/>
-        <CalcButton className="operator" value="-"/>
-        
-        <CalcButton value="C"/>
-        <CalcButton value="0"/>
-        <CalcButton value="="/>
-        <CalcButton className="operator" value="+"/>
-      </div>
-    )
+  const [calc, setCalc] = React.useState({
+    current: "0",
+    total: "0",
+    isInitial: true
+  });
+  
+  function handleNumber(value){
+    // alert('handle number click' + value)
+    let newValue = value;
+    
+    if (!calc.isInitial) {
+      newValue = calc.current + value;
+    }
+    
+    setCalc({current: newValue, total: calc.total, isInitial: false});
   }
-  function CalcButton(props) {
-    return <button className={props.className}>{props.value}</button>
+  
+  function handleOperator(value){
+    
   }
-  ReactDOM.render(<div className="app-container"><Calculator/></div>, document.getElementById("root"));
+  
+  function renderDisplay () {
+    return calc.current;
+  }
+  
+  return (
+    <div className="calculator">
+      <div className="display">{renderDisplay()}</div>
+      <CalcButton value="7" onClick={handleNumber}/>
+      <CalcButton value="8" onClick={handleNumber}/>
+      <CalcButton value="9" onClick={handleNumber}/>
+      <CalcButton className="operator" onClick={handleOperator} value="/"/>
+      
+      <CalcButton value="6" onClick={handleNumber}/>
+      <CalcButton value="5" onClick={handleNumber}/>
+      <CalcButton value="4" onClick={handleNumber}/>
+      <CalcButton className="operator" onClick={handleOperator} value="/" value="*"/>
+      
+      <CalcButton value="3" onClick={handleNumber}/>
+      <CalcButton value="2" onClick={handleNumber}/>
+      <CalcButton value="1" onClick={handleNumber}/>
+      <CalcButton className="operator" onClick={handleOperator} value="/" value="-"/>
+      
+      <CalcButton value="C"/>
+      <CalcButton value="0" onClick={handleNumber}/>
+      <CalcButton value="="/>
+      <CalcButton className="operator" onClick={handleOperator} value="/" value="+"/>
+    </div>
+  )
+}
+function CalcButton(props) {
+  return <button className={props.className} onClick={() => props.onClick(props.value)}>{props.value}</button>
+}
+ReactDOM.render(<div className="app-container"><Calculator/></div>, document.getElementById("root"));
